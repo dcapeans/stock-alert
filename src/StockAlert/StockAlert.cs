b5f062ad94a8de32fd31104c59b5994ed21110ef
Daniel Capeans <dcapeans@gmail.com>
2021-10-03 15:20:33 -0300

    create SetInterval

---
+++ src/StockAlert/StockAlert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockAlert
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Press ENTER to end program");
+      Task task = SetInterval(() => Console.WriteLine("Hello from SetInterval"), TimeSpan.FromSeconds(2));
+
+      Console.ReadLine();
+    }
+
+    private static async Task SetInterval(Action action, TimeSpan interval)
+    {
+      await Task.Delay(interval).ConfigureAwait(false);
+
+      action();
+
+      await SetInterval(action, interval);
+    }
+  }
+}
