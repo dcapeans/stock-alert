a9303f1656080a8494222b49d0169a28bca50130
Daniel Capeans <dcapeans@gmail.com>
2021-10-03 19:12:00 -0300

    request response parsing

--- src/StockAlert/HGAPIClient.cs
+++ src/StockAlert/HGAPIClient.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using dotenv.net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace StockAlert
 {
@@ -18,12 +22,29 @@ namespace StockAlert
       Url = url + $"/stock_price?key={this.Key}&symbol=bidi4";
     }
 
-    public async void GetStockPrice()
+    public void GetStockPrice()
     {
-      string response = await this.GetStringAsync(this.Url);
+      var response = this.GetStringAsync(this.Url).Result;
 
-      //Console.WriteLine(response);
-      Console.WriteLine(this.Url);
+      ApiResult responseJson = JsonConvert.DeserializeObject<ApiResult>(response);
+
+      Console.WriteLine(responseJson.results.price);
     }
+  }
+
+  public class ApiResult
+  {
+    public ApiResult()
+    {
+      this.results = new StockInfo();
+    }
+    [JsonProperty(PropertyName = "results")]
+    public StockInfo results { get; set; }
+  }
+
+  public class StockInfo
+  {
+    [JsonProperty(PropertyName = "price")]
+    public double price { get; set; }
   }
 }
